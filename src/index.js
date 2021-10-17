6835ce8e369942dc318d0c0f55d2941ae2d08690
LẬP NGUYỄN CHÍ <18120435@student.hcmus.edu.vn>
2021-10-17 11:23:15 +0700

    Refactor code : create route, app controller

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-//
+const route = require('./routes');
+
+//USE
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 
 //HTTP logger
 app.use(morgan('combined'));
@@ -15,17 +22,13 @@ app.use(morgan('combined'));
 app.engine('hbs', handlebars({
   extname: '.hbs'
 }));
+
 app.set('view engine', 'hbs'); //set view engine = handlebars
 app.set('views', path.join(__dirname, 'resources/views'));
 
+// Route init
+route(app);
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/new', (req, res) => {
-  res.render('new');
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
